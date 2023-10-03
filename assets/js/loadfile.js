aa671f32bd25636b8e92a24c53746a425050b7f5
JituRaj007 <jitendragupta.xhtmlchop@gmail.com>
2023-10-03 12:16:29 +0530

    Create New Project Setup

---
+++ assets/js/loadfile.js
@@ -0,0 +1,16 @@
+(function($) {
+/*	function sidebarResize() {
+		var sidebarWd = $('.sidebar').innerWidth();
+		$('.content-sec').css('padding-left' , sidebarWd);
+	}
+	$(window).on('load resize' , function() {
+    	sidebarResize();
+    });
+*/
+    $(document).ready(function() {
+    	/*Menu sidebar*/
+    	   
+    });
+
+
+})(jQuery);
